be7ba8d98fd39602bce11548a1850a87c277c103
spaceKSRW <kannuurawat18@outlook.com>
2024-07-24 20:51:57 +0530

    map_rendering

--- secon_react_app/src/App.jsx
+++ secon_react_app/src/App.jsx
@@ -4,10 +4,15 @@ import ClockHeading from './components/ClockHeading'
 import ClockSlogan from './components/ClockSlogan'
 import CurrentTime from './components/CurrentTime'
 function App() {
+  let data =['karan','anita reywhat','narender rawat','akanksha','nigga']
   return <div>
   <ClockHeading></ClockHeading>
   <ClockSlogan></ClockSlogan>
   <CurrentTime></CurrentTime>
+  <ul>
+    My family members
+    {data.map((item)=><li key={item} className='list-group-data'> {item}</li>)}
+  </ul>
   </div>
 }
 
