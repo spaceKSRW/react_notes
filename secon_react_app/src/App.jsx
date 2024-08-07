cc6dd881f5c389013646600539a547ebc89af62a
spaceKSRW <kannuurawat18@outlook.com>
2024-08-07 22:19:43 +0530

    conditional_changes

--- secon_react_app/src/App.jsx
+++ secon_react_app/src/App.jsx
@@ -4,15 +4,17 @@ import ClockHeading from './components/ClockHeading'
 import ClockSlogan from './components/ClockSlogan'
 import CurrentTime from './components/CurrentTime'
 function App() {
-  let data =['karan','anita reywhat','narender rawat','akanksha','nigga']
+  let data = ['karan', 'anita reywhat', 'narender rawat', 'akanksha', 'nigga']
   return <div>
-  <ClockHeading></ClockHeading>
-  <ClockSlogan></ClockSlogan>
-  <CurrentTime></CurrentTime>
-  <ul>
-    My family members
-    {data.map((item)=><li key={item} className='list-group-data'> {item}</li>)}
-  </ul>
+    <ClockHeading></ClockHeading>
+    <ClockSlogan></ClockSlogan>
+    <CurrentTime></CurrentTime>
+    <ul>
+      My family members
+      {data.length == 0 ? <h2> mai bhi toh anath uh , mujhe nahi khilaogi?</h2> : null}
+      {data.length == 0 && <h1> haan maibol rha hu ki mai bhi toh anath hu </h1>}
+        {data.map((item) => <li key={item} className='list-group-data'> {item}</li>)}
+      </ul>
   </div>
 }
 
